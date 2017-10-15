65d5d5fd07e80d868651628cbd01cb325c061af7
ikuisma <ilkka.a.kuisma@gmail.com>
2017-10-15 12:10:32 +0300

    Handlers and webhook implementation.

---
+++ herokubot.py
@@ -0,0 +1,42 @@
+import os
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+
+
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    level=logging.INFO)
+logger = logging.getLogger('herokubot')
+
+
+TOKEN = os.environ.get('TELEGRAM_API_TOKEN')
+PORT = int(os.environ.get('PORT', '5000'))
+
+updater = Updater(TOKEN)
+dispatcher = updater.dispatcher
+
+
+def start(bot, update):
+    bot.send_message(chat_id=update.message.chat_id, text="I'm a bot, please talk to me!")
+
+
+def echo(bot, update):
+    bot.send_message(chat_id=update.message.chat_id, text="Sorry, the application is not yet ready. Shoo! ")
+
+
+def unknown(bot, update):
+    bot.send_message(chat_id=update.message.chat_id, text="Sorry, I didn't understand that command.")
+
+
+start_handler = CommandHandler('start', start)
+echo_handler = MessageHandler(Filters.text, echo)
+unknown_handler = MessageHandler(Filters.command, unknown)
+
+dispatcher.add_handler(start_handler)
+dispatcher.add_handler(echo_handler)
+dispatcher.add_handler(unknown_handler)
+
+
+updater.start_webhook(listen="0.0.0.0",
+                      port=PORT,
+                      url_path=TOKEN)
+updater.bot.set_webhook("https://<appname>.herokuapp.com/" + TOKEN)
+updater.idle()
